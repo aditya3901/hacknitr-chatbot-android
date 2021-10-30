64bda8d083cc31122632bd0630b7607def6048ad
aditya3901 <2020itb058.aditya@students.iiests.ac.in>
2021-10-30 19:01:55 +0530

    hacknitr chatbot android

---
+++ app/src/main/java/com/project/alexia/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.project.alexia.utils
+
+object Constants {
+
+    const val SEND_ID = "SEND_ID"
+    const val RECEIVE_ID = "RECEIVE_ID"
+
+    const val OPEN_GOOGLE = "Opening Google..."
+    const val OPEN_SEARCH = "Searching..."
+    const val OPEN_SEARCH_HAPPY = "I've listed some happy songs for you. Please head on to Recommended Songs section from menu."
+    const val OPEN_SEARCH_SAD = "I've listed some songs to lift your mood. Please head on to Recommended Songs section from menu."
+}
